4d14e3255b79d1719e42cbf18bcdeda183aad7ec
rifaifirdaus <rifaimuhammad2004@gmail.com>
2026-08-28 23:32:52 +0700

    initial staging2

--- public/index.php
+++ public/index.php
@@ -10,32 +10,10 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-| Shared hosting: file Laravel ada di luar public_html
-| Path: /home/username/agusmeubel/
-| public_html/ berisi isi dari folder public/ Laravel
-|
-| Sesuaikan path __DIR__.'/../vendor' dengan lokasi install kamu.
-| Contoh jika struktur hosting:
-|   /home/username/agusmeubel/       <- root Laravel
-|   /home/username/public_html/      <- isi folder public/
-|
-| Maka path di bawah sudah benar karena index.php ada di public_html
-| dan Laravel root ada satu level di atas (/../agusmeubel)
-|
-*/
-
+// Register The Auto Loader...
 require __DIR__.'/../vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-*/
-
+// Run The Application...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
